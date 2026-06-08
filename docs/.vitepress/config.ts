d5cbfd3c5e6b2ca195db5029d422552a5267f3d7
Lu1z4-o <oluiza441@gmail.com>
2026-06-08 15:11:43 -0300

    ...

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitepress';
 
-// refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
   title: 'Escola de Idiomas Vision',
@@ -10,21 +9,11 @@ export default defineConfig({
     nav: [
       { text: 'Página inicial', link: '/index' },
 
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
-
-      // ...
+      
     ], 
 
     sidebar: [
       {
-        // text: 'Guide',
         items: [
           { text: 'Página inicial', link: '/index'},
           {text: '1. Introdução', link: '/introducao'},
@@ -37,7 +26,6 @@ export default defineConfig({
           {text: '8. Riscos e mitigações', link: '/riscos'}, 
           {text: '9. Custos e orçamento', link: '/orcamento'}, 
           {text: '10. Considerações finais', link: '/consideracoes'}
-          // ...
         ],
       },
     ],
